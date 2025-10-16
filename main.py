3b9291fee8b33ae824b5e786dfe0f12d8f4b2729
GatorSindre <olsensindre51@gmail.com>
2025-10-16 14:01:27 +0200

    added blink variation

--- main.py
+++ main.py
@@ -2,38 +2,6 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import pyautogui
-import subprocess
-
-cameraNum = input("CameraNum: ")
-cameraNum = int(cameraNum)
-scrollType = input("Scroll on laptop[0] or phone[1] or both[2]\n  ")
-scrollType = int(scrollType)
-
-if not scrollType >= 0 and scrollType <= 2:
-    print("error: invalid scrollType")
-    exit()
-
-if cameraNum < 0 or cameraNum > 100:
-    exit()
-
-def adb_command(cmd):
-    full_cmd = f"adb shell {cmd}"
-    subprocess.run(full_cmd, shell=True)
-
-def swipe(x1, y1, x2, y2, duration=500):
-    adb_command(f"input swipe {x1} {y1} {x2} {y2} {duration}")
-
-def DoAswipe(scrollType):
-    if scrollType == 0:
-        pyautogui.press("down")
-    elif scrollType == 1:
-        swipe(500, 1800, 500, 500, 100)
-    elif scrollType == 2:
-        pyautogui.press("down")
-        swipe(500, 1800, 500, 500, 100)
-    else:
-        print("Invalid scrollType\nexiting...")
-        exit()
 
 mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh(max_num_faces=1)
@@ -49,11 +17,14 @@ def eye_aspect_ratio(landmarks, eye_points, img_w, img_h):
     EAR = (vertical1 + vertical2) / (2.0 * horizontal)
     return EAR
 
-cap = cv2.VideoCapture(cameraNum)
+cap = cv2.VideoCapture(0)
 
 BLINK_THRESH = 0.21
-CLOSED_FRAMES = 3
-frame_counter = 0
+FIRST_CLOSED_FRAMES = 1
+SECOND_CLOSED_FRAMES = 5
+THIRD_CLOSED_FRAMES = 9
+Frame_Counter = 9
+NumberMode = False
 
 while True:
     success, frame = cap.read()
@@ -69,13 +40,42 @@ while True:
         right_EAR = eye_aspect_ratio(landmarks, RIGHT_EYE, img_w, img_h)
         avg_EAR = (left_EAR + right_EAR) / 2.0
         
-        if avg_EAR < BLINK_THRESH:
-            frame_counter += 1
-        else:
-            if frame_counter >= CLOSED_FRAMES:
-                DoAswipe(scrollType)
-            frame_counter = 0
 
+        if NumberMode == True: 
+            if avg_EAR < BLINK_THRESH:
+                Frame_Counter += 1
+            elif Frame_Counter >= THIRD_CLOSED_FRAMES:
+                    pyautogui.press("3")
+                    print("Blink Third - Numbers mode")
+                    Frame_Counter = 0
+                    NumberMode = False
+            elif Frame_Counter >= SECOND_CLOSED_FRAMES:
+                    pyautogui.press("2")
+                    print("Blink Second - Numbers mode")
+                    Frame_Counter = 0    
+                    NumberMode = False
+            elif Frame_Counter >= FIRST_CLOSED_FRAMES:
+                    pyautogui.press("1")
+                    print("Blink First - Numbers mode")
+                    Frame_Counter = 0
+                    NumberMode = False
+        else:
+            if avg_EAR < BLINK_THRESH:
+                Frame_Counter += 1
+            elif Frame_Counter >= THIRD_CLOSED_FRAMES:
+                    NumberMode = True
+                    print("Blink Third")
+                    Frame_Counter = 0
+            elif Frame_Counter >= SECOND_CLOSED_FRAMES:
+                    pyautogui.press("up")
+                    print("Blink Second")
+                    Frame_Counter = 0    
+            elif Frame_Counter >= FIRST_CLOSED_FRAMES:
+                    pyautogui.press("down")
+                    print("Blink First")
+                    Frame_Counter = 0     
+
+            
     cv2.imshow("Blink Detection", frame)
     if cv2.waitKey(1) & 0xFF == 27:
         break
